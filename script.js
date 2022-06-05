b76a5b42d5eb468c5b726e1b8863e0ab3d492e4a
Ziad Hamdy <ziadhamdy268@gmail.com>
2022-06-05 03:11:35 +0200

    Last modification Changed the layout of the page Added buttons for size, colors ,and resetting the palette Added reset.css for inconsistencies

--- script.js
+++ script.js
@@ -1,22 +1,63 @@
-let mainDiv = document.querySelector("#main");
+let color = "black";
+let mouseClicked = true;
 
+function createBoard(size){
+    let board = document.querySelector(".board");
+    let divs = board.querySelectorAll("div");
+    divs.forEach((div) => div.remove());
+    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
-for(let i=0; i<16; i++){
-    let innerDiv = document.createElement("div");
-    for(let j=0; j<16; j++){
+    for(let i=0; i<size*size; i++){
         let div = document.createElement("div");
-        div.classList.add("grid");
-        innerDiv.appendChild(div);
+        div.addEventListener("mouseover", colorDiv);
+        board.insertAdjacentElement("beforeend", div);
     }
-    mainDiv.appendChild(innerDiv);
 }
-const divs = document.querySelectorAll(".grid");
-divs.forEach((div) => {
-    div.addEventListener("mouseover", function(){
-        console.log("inside");
-        div.style.backgroundcolor = "blue";
-        setTimeout(function(){
-            div.style.backgroundcolor = "";
-        }, 500);
-    }, false);
+
+createBoard(16);
+
+function changeSize(newSize){
+    let errorMessage = document.querySelector("#error");
+    if(newSize >= 2 && newSize <= 100){
+        errorMessage.style.display = "none";
+        createBoard(newSize);
+    }
+    else{
+        errorMessage.style.display = "flex";
+    }
+}
+
+function colorDiv(){
+    if(mouseClicked){
+        if(color === "random"){
+            this.style.backgroundColor = `hsl(${Math.random()*360}, 100%, 50%)`;
+        }
+        else{
+            this.style.backgroundColor = color;
+        }
+    }
+}
+
+function changeColor(newColor){
+    color = newColor;
+}
+
+function resetBoard(){
+    let board = document.querySelector(".board");
+    let divs = board.querySelectorAll("div");
+    divs.forEach((div) => div.style.backgroundColor = "white");
+}
+
+document.querySelector("body").addEventListener("click", (e) =>{
+    if(e.target.tagName != "BUTTON"){
+        mouseClicked = !mouseClicked;
+        let mode = document.querySelector(".mode");
+        if(mouseClicked){
+            mode.textContent = "Mode: Coloring";
+        }
+        else{
+            mode.textContent = "Mode: Not Coloring";
+        }
+    }
 });
